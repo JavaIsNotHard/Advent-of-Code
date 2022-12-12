a017e249adaa35650ee226bf41c771bdb74b2daa
JavaIsNotHard <jibeshshrestha00@gmail.com>
2022-12-12 13:13:32 +0545

    Day 01

--- 2022/day01/src/solution.cpp
+++ 2022/day01/src/solution.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <array>
 #include <vector>
 
 bool readFile();
 void partOne();
 
 
-bool readFile (const std::string& FileName, std::vector<std::string>& lines) {
+bool readFile (const std::string& FileName, std::vector<std::string>& calorie) {
     std::ifstream inf {FileName};
     if (!inf) {
         std::cerr << "Couldn't open file";
@@ -17,16 +18,16 @@ bool readFile (const std::string& FileName, std::vector<std::string>& lines) {
     // while the list of string is not empty
     while (std::getline(inf, dataString)) {
         // put the strings in a array
-        lines.push_back(dataString);
+        calorie.push_back(dataString);
     }
     return true;
 }
 
-void partOne(std::vector<std::string>& lines) {
+void partOne(std::vector<std::string>& calorie) {
     // maximum amount of size max and sum can hold in this case 8 bytes
     size_t max {};
     size_t sum {};
-    for (auto line : lines) {
+    for (auto line : calorie) {
         if (!line.empty()) 
             // stoul converts a string to an unsinged integer
             // and sum out all the numbers until we find a whitespace or a empty line
@@ -42,6 +43,34 @@ void partOne(std::vector<std::string>& lines) {
     std::cout << max << '\n';
 }
 
+void partTwo(std::vector<std::string>& calorie) {
+    std::array<int, 3> topThree {};
+    int max1 {};
+    int max2 {};
+    int max3 {};
+    int sum {};
+    for (auto line : calorie) {
+        if (!line.empty()) {
+            sum += std::stoul(line);
+        }
+        else {
+            if (sum > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = sum;
+            } else if (sum > max2) {
+                max3 = max2;
+                max2 = sum;
+            } else if (sum > max3) 
+                max3 = sum;
+            sum = 0;
+        }
+    }
+    int total = max1 + max2 + max3;
+    std::cout << "The Top Three Are: " << max1 << ' ' << max2 << ' ' << max3 << '\n';
+    std::cout << "The Total Sum: " << total << '\n';
+}
+
 // we are passing the name of the file as command line argument
 int main(int argc, char* argv[]) {  
     std::vector<std::string> lines{};
@@ -50,5 +79,6 @@ int main(int argc, char* argv[]) {
             return EXIT_FAILURE;
     }
     partOne(lines);
+    partTwo(lines);
     return 0;
 }
